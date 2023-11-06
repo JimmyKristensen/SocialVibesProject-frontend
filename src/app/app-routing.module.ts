af97085d09ed1d30b64bbd79be8b3c970740542f
JimmyKristensen <siswanto@live.dk>
2023-11-06 13:27:53 +0100

    Removed tap3 and and added frontpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'frontpage',
+    loadChildren: () => import('./frontpage/frontpage.module').then( m => m.FrontpagePageModule)
   }
 ];
 @NgModule({
