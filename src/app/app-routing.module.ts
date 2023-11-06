900d11b05ab171be2c425843d30090a0e5db583c
Healsu <timmie2500@gmail.com>
2023-11-06 14:28:46 +0100

    Start Setup for frontpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+ {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'frontpage',
     loadChildren: () => import('./frontpage/frontpage.module').then( m => m.FrontpagePageModule)
   }
 ];
