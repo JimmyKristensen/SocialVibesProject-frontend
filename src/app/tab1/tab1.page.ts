dbdb70498ae689a808f76c714c95587599113f94
Healsu <timmie2500@gmail.com>
2023-11-09 09:29:44 +0100

    Simple onepager for now, works for chatTabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  selectedTab: string = 'Friends'; // Default to Friends tab
 
   constructor() {}
 
+  showContent(tab: string) {
+    this.selectedTab = tab;
+  }
 }
