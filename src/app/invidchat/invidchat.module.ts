1b813c8ef2b43f42b564cdf46383027adceb05eb
Healsu <timmie2500@gmail.com>
2023-11-09 10:28:15 +0100

    Version 1

---
+++ src/app/invidchat/invidchat.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { InvidchatPageRoutingModule } from './invidchat-routing.module';
+
+import { InvidchatPage } from './invidchat.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    InvidchatPageRoutingModule
+  ],
+  declarations: [InvidchatPage]
+})
+export class InvidchatPageModule {}
