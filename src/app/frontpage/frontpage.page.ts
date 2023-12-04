e20fa43cb6b18f76591fd8a768369fb628a5ad7d
Healsu <timmie2500@gmail.com>
2023-12-04 11:48:29 +0100

    Version 1 - added new functions and html

--- src/app/frontpage/frontpage.page.ts
+++ src/app/frontpage/frontpage.page.ts
@@ -2,7 +2,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { InvidCallService } from '../calls/invid-call.service';
-import { InvidService } from '../testing/invid.service';
+import { GetAllUsersService } from '../calls/get-all-users.service';
+
+interface UserData {
+  Name: string;
+  TimeStamp: string;
+}
 
 @Component({
   selector: 'app-frontpage',
@@ -10,25 +15,40 @@ import { InvidService } from '../testing/invid.service';
   styleUrls: ['frontpage.page.scss'],
 })
 export class FrontpagePage {
+  allUsersData: { [key: string]: UserData } = {}; // Type for user data
+  selectedUserId: string = '';
+
   constructor(
     private router: Router,
     private invidCallService: InvidCallService,
-    private invidService: InvidService
+    private getAllUsersService: GetAllUsersService,
   ) {}
 
   login() {
-    console.log("Calling Fetch");
+    console.log('Calling Fetch');
     this.fetchData();
-    console.log("Logging in");
+    console.log('Logging in');
     // Do not navigate to 'tabs/tab1' here
   }
 
   fetchData() {
-    this.invidCallService.getInvidChats().subscribe(data => {
+    this.invidCallService.getInvidChats().subscribe((data) => {
       console.log('Test data given: ', data);
-      this.router.navigate(['./tabs/tab1'], { queryParams: { invidChatsData: JSON.stringify(data) } });
+      this.router.navigate(['./tabs/tab1'], {
+        queryParams: { invidChatsData: JSON.stringify(data) },
+      });
     });
+  }
 
-  
-}
+  ngOnInit() {
+    this.getAllUsersService.getAllUsers().subscribe((data) => {
+      console.log('All users: ', data);
+      this.allUsersData = data.profiles;
+    });
+  }
+
+  selectUser(userId: string) {
+    console.log('Selected user ID:', userId);
+    
+  }
 }
